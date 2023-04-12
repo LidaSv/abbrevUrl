421a4fd3bc58b710c8d9e2597fb984f824d4d3de
ldsviyazova <ldsviyazova@MSK-FVFH71G0Q05P.local>
2023-04-12 21:32:28 +0300

    Инкремент 8

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,13 +3,11 @@ package server
 import (
 	"abbrevUrl/internal/app"
 	"abbrevUrl/internal/storage"
-	"compress/gzip"
 	"context"
 	"errors"
 	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/spf13/pflag"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -23,38 +21,10 @@ type Config struct {
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/cache"`
 }
 
-type gzipWriter struct {
-	http.ResponseWriter
-	Writer io.Writer
-}
-
-func (w gzipWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
-}
-
-func gzipHandle(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-		defer gz.Close()
-
-		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
-	})
-}
-
 func AddServer() {
 	r := chi.NewRouter()
 
-	r2 := gzipHandle(r)
+	r2 := GzipHandle(r)
 
 	st := storage.Iter()
 	var cfg Config
