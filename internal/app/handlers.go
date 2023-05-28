59086cae9f1eebff0913ea634aa28b2378f0e428
LidaSv <slidad1997@yandex.ru>
2023-05-28 23:41:44 +0300

    Инкремент 14. Исправление ошибки iteration14_test.go:351: Не удалось дождаться удаления переданных URL в течении отведенного времени

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -59,6 +59,301 @@ func HelpHandler(url Storage) *Hand {
 
 type ShortURL []string
 
+//func (s *Hand) l(w http.ResponseWriter, r *http.Request) {
+//	_, err := getCookies(r)
+//	if err != nil {
+//		fmt.Fprint(w, err)
+//		return
+//	}
+//
+//	shortURLByte, err := middleware.ReadBody(w, r)
+//	defer r.Body.Close()
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write(shortURLByte)
+//		return
+//	}
+//
+//	var t ShortURL
+//	err = json.Unmarshal(shortURLByte, &t)
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		log.Fatal("Unmarshal: ", err)
+//	}
+//
+//	param := "{" + strings.Join(t, ",") + "}"
+//	db := s.url.DatabaseDsns(param)
+//	if db == nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write([]byte("нет коннекта с БД"))
+//		return
+//	}
+//
+//	deleteSignal := make(chan struct{}, 1)
+//
+//	go func() {
+//		ticker := time.NewTicker(3 * time.Second)
+//		defer ticker.Stop()
+//
+//		for {
+//			select {
+//			case <-deleteSignal:
+//				return
+//			case <-ticker.C:
+//				err := deleteFromDB(context.Background(), db)
+//				if err != nil {
+//					log.Println("delete:", err)
+//				}
+//			}
+//		}
+//	}()
+//
+//	groupSize := 50
+//
+//	var wg sync.WaitGroup
+//	wg.Add(len(t))
+//
+//	for i := 0; i < len(t); i += groupSize {
+//		end := i + groupSize
+//		if end > len(t) {
+//			end = len(t)
+//		}
+//
+//		group := t[i:end]
+//
+//		go func(group ShortURL) {
+//			defer wg.Done()
+//
+//			param := "{" + strings.Join(group, ",") + "}"
+//
+//			_, err := db.Exec(context.Background(), "update long_short_urls set flg_delete = 1 where short_url = any($1)", param)
+//			if err != nil {
+//				log.Println("update:", err)
+//			}
+//
+//			deleteSignal <- struct{}{}
+//		}(group)
+//	}
+//
+//	wg.Wait()
+//
+//	deleteSignal <- struct{}{}
+//	<-deleteSignal
+//
+//	err = deleteFromDB(context.Background(), db)
+//	if err != nil {
+//		log.Println("final delete:", err)
+//	}
+//
+//	w.WriteHeader(http.StatusAccepted)
+//}
+
+//func (s *Hand) DeleteShortLink(w http.ResponseWriter, r *http.Request) {
+//	_, err := getCookies(r)
+//	if err != nil {
+//		fmt.Fprint(w, err)
+//		return
+//	}
+//
+//	shortURLByte, err := middleware.ReadBody(w, r)
+//	defer r.Body.Close()
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write(shortURLByte)
+//		return
+//	}
+//
+//	var t ShortURL
+//	err = json.Unmarshal(shortURLByte, &t)
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		log.Fatal("Unmarshal: ", err)
+//	}
+//
+//	param := "{" + strings.Join(t, ",") + "}"
+//	db := s.url.DatabaseDsns(param)
+//
+//	if db == nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write([]byte("нет коннекта с БД"))
+//		return
+//	}
+//
+//	// Create a buffered channel to receive results from worker goroutines
+//	resultCh := make(chan error, len(t))
+//
+//	// Define the number of worker goroutines to process the updates concurrently
+//	numWorkers := 10
+//
+//	// Create a wait group to wait for all worker goroutines to finish
+//	var wg sync.WaitGroup
+//	wg.Add(numWorkers)
+//
+//	// Create a context for cancellation
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+//
+//	groupSize := 50
+//	for i := 0; i < numWorkers; i++ {
+//		go func() {
+//			defer wg.Done()
+//
+//			for i := 0; i < len(t); i += groupSize {
+//				end := i + groupSize
+//				if end > len(t) {
+//					end = len(t)
+//				}
+//
+//				group := t[i:end]
+//
+//				param := "{" + strings.Join(group, ",") + "}"
+//
+//				_, err := db.Exec(context.Background(), "update long_short_urls set flg_delete = 1 where short_url = any($1)", param)
+//				if err != nil {
+//					resultCh <- err
+//					return
+//				}
+//			}
+//		}()
+//	}
+//
+//	// Wait for all worker goroutines to finish
+//	go func() {
+//		wg.Wait()
+//		close(resultCh)
+//	}()
+//
+//	// Perform the delete operation every 3 seconds
+//	go func() {
+//		ticker := time.NewTicker(3 * time.Second)
+//		defer ticker.Stop()
+//
+//		for {
+//			select {
+//			case <-ctx.Done():
+//				return
+//			case <-ticker.C:
+//				deleteErr := deleteFromDB(ctx, db)
+//				if deleteErr != nil {
+//					log.Fatal("delete: ", deleteErr)
+//				}
+//			case updateErr := <-resultCh:
+//				if updateErr != nil {
+//					log.Fatal("update: ", updateErr)
+//				}
+//				deleteErr := deleteFromDB(ctx, db)
+//				if deleteErr != nil {
+//					log.Fatal("delete: ", deleteErr)
+//				}
+//			}
+//		}
+//	}()
+//
+//	w.WriteHeader(http.StatusAccepted)
+//}
+//
+//func deleteFromDB(ctx context.Context, db *pgxpool.Pool) error {
+//	conn, err := db.Acquire(ctx)
+//	if err != nil {
+//		return err
+//	}
+//	defer conn.Release()
+//
+//	_, err = conn.Exec(ctx, "delete from long_short_urls where flg_delete = 1")
+//	return err
+//}
+
+//func (s *Hand) DeleteShortLink(w http.ResponseWriter, r *http.Request) {
+//	_, err := getCookies(r)
+//	if err != nil {
+//		fmt.Fprint(w, err)
+//		return
+//	}
+//
+//	shortURLByte, err := middleware.ReadBody(w, r)
+//	defer r.Body.Close()
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write(shortURLByte)
+//		return
+//	}
+//
+//	var t ShortURL
+//	err = json.Unmarshal(shortURLByte, &t)
+//	if err != nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		log.Fatal("Unmarshal: ", err)
+//	}
+//
+//	param := "{" + strings.Join(t, ",") + "}"
+//	db := s.url.DatabaseDsns(param)
+//
+//	if db == nil {
+//		w.WriteHeader(http.StatusBadRequest)
+//		w.Write([]byte("нет коннекта с БД"))
+//		return
+//	}
+//
+//	resultCh := make(chan error, len(t))
+//
+//	numWorkers := 10
+//
+//	var wg sync.WaitGroup
+//	wg.Add(numWorkers)
+//
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+//
+//	groupSize := 50
+//	for i := 0; i < numWorkers; i++ {
+//		go func() {
+//			defer wg.Done()
+//
+//			for i := 0; i < len(t); i += groupSize {
+//				end := i + groupSize
+//				if end > len(t) {
+//					end = len(t)
+//				}
+//
+//				group := t[i:end]
+//
+//				param := "{" + strings.Join(group, ",") + "}"
+//
+//				_, err := db.Exec(ctx, "update long_short_urls set flg_delete = 1 where short_url = any($1)", param)
+//				if err != nil {
+//					resultCh <- err
+//					return
+//				}
+//			}
+//		}()
+//	}
+//
+//	go func() {
+//		wg.Wait()
+//		close(resultCh)
+//	}()
+//
+//	ticker := time.NewTicker(3 * time.Second)
+//	defer ticker.Stop()
+//
+//	for {
+//		select {
+//		case <-ctx.Done():
+//			w.WriteHeader(http.StatusAccepted)
+//			return
+//		case <-ticker.C:
+//			deleteErr := deleteFromDB(ctx, db)
+//			if deleteErr != nil {
+//				log.Fatal("delete: ", deleteErr)
+//			}
+//		case updateErr := <-resultCh:
+//			if updateErr != nil {
+//				log.Fatal("update: ", updateErr)
+//			}
+//		}
+//	}
+//}
+
 func (s *Hand) DeleteShortLink(w http.ResponseWriter, r *http.Request) {
 	_, err := getCookies(r)
 	if err != nil {
@@ -81,68 +376,70 @@ func (s *Hand) DeleteShortLink(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Unmarshal: ", err)
 	}
 
-	param := "{" + strings.Join(t, ",") + "}"
-	db := s.url.DatabaseDsns(param)
+	db := s.url.DatabaseDsns(t[0])
+
 	if db == nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("нет коннекта с БД"))
 		return
 	}
 
-	deleteSignal := make(chan struct{}, 1)
+	resultCh := make(chan error, len(t))
+
+	numWorkers := 10
+
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	groupSize := 50
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < len(t); i += groupSize {
+				end := i + groupSize
+				if end > len(t) {
+					end = len(t)
+				}
+
+				group := t[i:end]
+
+				param := "{" + strings.Join(group, ",") + "}"
+
+				_, err := db.Exec(ctx, "update long_short_urls set flg_delete = 1 where short_url = any($1)", param)
+				if err != nil {
+					resultCh <- err
+					return
+				}
+			}
+		}()
+	}
+
+	deleteTicker := time.NewTicker(3 * time.Second)
+	defer deleteTicker.Stop()
 
 	go func() {
-		ticker := time.NewTicker(3 * time.Second)
-		defer ticker.Stop()
-
 		for {
 			select {
-			case <-deleteSignal:
+			case <-ctx.Done():
 				return
-			case <-ticker.C:
-				err := deleteFromDB(context.Background(), db)
-				if err != nil {
-					log.Println("delete:", err)
+			case <-deleteTicker.C:
+				deleteErr := deleteFromDB(ctx, db)
+				if deleteErr != nil {
+					log.Fatal("delete: ", deleteErr)
 				}
 			}
 		}
 	}()
 
-	groupSize := 50
-
-	var wg sync.WaitGroup
-	wg.Add(len(t))
-
-	for i := 0; i < len(t); i += groupSize {
-		end := i + groupSize
-		if end > len(t) {
-			end = len(t)
-		}
-
-		group := t[i:end]
-
-		go func(group ShortURL) {
-			defer wg.Done()
-
-			param := "{" + strings.Join(group, ",") + "}"
-
-			_, err := db.Exec(context.Background(), "update long_short_urls set flg_delete = 1 where short_url = any($1)", param)
-			if err != nil {
-				log.Println("update:", err)
-			}
-
-			deleteSignal <- struct{}{}
-		}(group)
-	}
-
 	wg.Wait()
 
-	deleteSignal <- struct{}{}
-	<-deleteSignal
-
-	err = deleteFromDB(context.Background(), db)
-	if err != nil {
-		log.Println("final delete:", err)
+	deleteErr := deleteFromDB(ctx, db)
+	if deleteErr != nil {
+		log.Fatal("delete: ", deleteErr)
 	}
 
 	w.WriteHeader(http.StatusAccepted)
